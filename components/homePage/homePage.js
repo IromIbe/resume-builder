325a574fc26a56e100c9b1669b23ac8bcbc7d1a6
IromIbe <iromibe@outlook.com>
2022-10-11 23:27:02 +0100

    fixed stuff

--- components/homePage/homePage.js
+++ components/homePage/homePage.js
@@ -7,12 +7,12 @@ function HomePage() {
   return (
     <HomeCont className=''>
       <div className='first-cont  bg-[#f2f5fa]   py-7 md:py-5 lg:py-0'>
-        <div className=' item flex sm:flex-row flex-col justify-evenly items-center mx-1'>
+        <div className=' item flex sm:flex-row flex-col justify-evenly items-center sm:mx-1 mx-3'>
           <div className='image'>
             <img src='/Resume.svg' alt='' style={{ minWidth: "280px" }} />
           </div>
           <div className='more-info justify-self-start'>
-            <h1 className='text-5xl font-bold leading-14 mb-6'>
+            <h1 className='sm:text-5xl text-4xl font-bold leading-14 mb-6'>
               Try Our Professional <br /> Resume Builder Now!
             </h1>
             <p className='mb-6 max-w-sm tracking-sm leading-7 text-base text-gray-700'>
@@ -32,15 +32,15 @@ function HomePage() {
         </div>
       </div>
       <div className='second-cont bg-white flex flex-col py-8 text-center'>
-        <p className='text-4xl font-semibold text-gray-800 mb-10 '>
+        <p className='sm:text-4xl text-3xl font-semibold text-gray-800 mb-10 '>
           Where are they getting hired?
         </p>
-        <div className='companies flex justify-around items-center flex-wrap  mb-12'>
-          <img src='/two.svg' alt='' />
-          <img src='/one.svg' alt='' />
-          <img src='/three.svg' alt='' />
-          <img src='/four.svg' alt='' />
-          <img src='/five.svg' alt='' />
+        <div className='companies flex justify-center items-center flex-wrap flex-1  mb-4'>
+          <img src='/two.svg' alt='' className='mx-2 mb-3' />
+          <img src='/one.svg' alt='' className='mx-2 mb-3' />
+          <img src='/three.svg' alt='' className='mx-2 mb-3' />
+          <img src='/four.svg' alt='' className='mx-2 mb-3' />
+          <img src='/five.svg' alt='' className='mx-2 mb-3' />
         </div>
         <div>
           <button className='text-white mb-5 bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-l from-blue-500 to-cyan-500  py-2 px-3 rounded-md text-base'>
